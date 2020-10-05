fa8da90f52977ec606136c69eb16ef3616634edc
omarattallah97 <32816430+ertomar@users.noreply.github.com>
2020-10-05 22:02:28 +0200

    finished

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,100 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  notifNumber: Observable<any[]>;
+  notificationNumber: number;
+  posts: Observable<any[]>;
+  constructor(
+    public db: AngularFireDatabase,
+    public fireStore: AngularFirestore
+  ) {
+    db.list("notifications")
+      .valueChanges()
+      .subscribe((num) => {
+        this.notificationNumber = (num[0] as number) || 0;
+      });
 
-  constructor() {}
-
+    this.posts = db.list("posts").valueChanges();
+    this.posts = this.db
+      .list("posts")
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({
+            key: c.payload.key,
+            ...(c.payload.val() as object),
+          }))
+        )
+      );
+  }
+  addPost(text: string = "Post") {
+    this.db
+      .list("posts")
+      .push({ text: text })
+      .then((_) => console.log("success"))
+      .catch((err) => console.log(err, "You do not have access!"));
+    this.changeNotification();
+  }
+  deletePost(key: string) {
+    this.db.list("posts").remove(key);
+  }
+  addPostLike(key: string) {
+    let inc = true;
+    this.db.database
+      .ref(`posts/${key}/likes`)
+      .transaction(function (current_value) {
+        if ((current_value || 0) === 0) {
+          return (current_value || 0) + 1;
+        } else {
+          inc = false;
+          return (current_value || 0) - 1;
+        }
+      });
+    this.changeNotification(inc);
+  }
+  addPostComment(key: string, commentText: string | number) {
+    this.db.list(`posts/${key}/comments`).push({ text: commentText });
+    this.changeNotification();
+  }
+  addCommentLike(postKey: string, commentKey: string) {
+    let inc = true;
+    this.db.database
+      .ref(`posts/${postKey}/comments/${commentKey}/likes`)
+      .transaction(function (current_value) {
+        if ((current_value || 0) == 0) {
+          return (current_value || 0) + 1;
+        } else {
+          inc = false;
+          return (current_value || 0) - 1;
+        }
+      });
+    this.changeNotification(inc);
+  }
+  clearNotifications() {
+    this.db.list("notifications").remove();
+  }
+  changeNotification(inc = true) {
+    this.db.database
+      .ref(`notifications/number`)
+      .transaction(function (current_value) {
+        let value: number = 1;
+        if (!inc) {
+          value = -1;
+        }
+        if ((current_value || 0) + value >= 0) {
+          return (current_value || 0) + value;
+        } else {
+          return 0;
+        }
+      });
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD6TdStj7g29pmj14hg1b1yZUezvoxd6AU",
+    authDomain: "fir-9d3e9.firebaseapp.com",
+    databaseURL: "https://fir-9d3e9.firebaseio.com",
+    projectId: "fir-9d3e9",
+    storageBucket: "fir-9d3e9.appspot.com",
+    messagingSenderId: "122669682166",
+    appId: "1:122669682166:web:f2dea19ab5e09f64568bd7",
+    measurementId: "G-889W9KH1FD",
+  },
 };
